419929826d0fff02648968f4551fcab37a74acbd
leonardo <leonardoyamagutione@gmail.com>
2025-01-20 19:31:10 -0300

    Ajustes de identação

--- src/test/java/PassagemTest.java
+++ src/test/java/PassagemTest.java
@@ -1,5 +1,3 @@
-
-
 // 1 - biblioteca / imports
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,15 +10,13 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-
 // 2 - Classe
 public class PassagemTest {
 
     // 2.1 - Atributos
     private WebDriver driver; // objeto do Selenium
 
-    // 2.2 - Funções e Métodos
-
+    // 2.2 - Funções e Métodos//
 
     // Antes dos Teste
     @BeforeEach
@@ -31,18 +27,17 @@ public class PassagemTest {
 
     }
 
-
     // Depois dos Teste
     @AfterEach
-    public void finalizar(){
-        driver.quit(); //destroi o objeto do selenium
+    public void finalizar() {
+        driver.quit(); // destroi o objeto do selenium
 
     }
 
     // Teste
     @Test
     public void comprarPassagem() {
-        driver.get("https://www.blazedemo.com"); //abre o site
+        driver.get("https://www.blazedemo.com"); // abre o site
         // seleciona origem, destino e aperta botão "Fing Flights"
         driver.findElement(By.name("fromPort")).click(); // clica no combo
         {
@@ -52,7 +47,7 @@ public class PassagemTest {
 
         // combo Destino
         {
-            WebElement  dropdown = driver.findElement(By.name("toPort"));
+            WebElement dropdown = driver.findElement(By.name("toPort"));
             dropdown.click();
             dropdown.findElement(By.xpath("//option[. = 'Cairo']")).click();
         }
@@ -61,24 +56,18 @@ public class PassagemTest {
         driver.findElement(By.cssSelector("input.btn.btn-primary")).click();
 
         // Transição de Página//
-    
+
         // Verifica se foi selecionado minha cidade.
-        assertEquals("Flights from São Paolo to Cairo:", 
-            driver.findElement(By.cssSelector("h3")).getText());
+        assertEquals("Flights from São Paolo to Cairo:",
+                driver.findElement(By.cssSelector("h3")).getText());
 
         // clica no botão de voo desejado.
         driver.findElement(By.cssSelector("tr:nth-child(1) .btn")).click();
 
-
         // Verifica se entrou no agendamento de voo correto
         assertEquals("Your flight from TLV to SFO has been reserved.",
-            driver.findElement(By.cssSelector("h2")).getText());
-        
+                driver.findElement(By.cssSelector("h2")).getText());
 
-
-
-    
     } // Final do comprarPassagem
-
 
 } // Final da classe PassagemTest
